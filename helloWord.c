9a2d8c265ebaa2ee11db0bbd103f56ce368aea1a
floback <git@github.com:floback/Codigos-c.git>
2020-04-09 12:58:27 -0300

    Update HelloWord

---
+++ helloWord.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+//  ==========                 ==========
+//  ========== #helpdeskloback ==========
+//  ==========                 ==========
+
+int main(){
+    //imprime a mensagem que estive entre aspas duplas
+    printf("Ola Mundo!\n");
+
+    //valor de retorno para a função principal
+    //indicando que o programa acabou
+    return 0;
+}
