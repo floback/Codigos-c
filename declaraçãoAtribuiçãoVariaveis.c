50801fe3d206fc40bb5c005ce06aebc5a66303d4
floback <git@github.com:floback/Codigos-c.git>
2020-05-04 12:45:51 -0300

    Update Declaração Variaveis e atribuiçõs

---
+++ declaraçãoAtribuiçãoVariaveis.c
@@ -0,0 +1,22 @@
+//  ==========                 ==========
+//  ========== #helpdeskloback ==========
+//  ==========                 ==========
+
+#include <stdlib.h>
+
+int main()
+{
+	int idade;
+	float salario, valor;
+	idade = 25;
+	valor =  4500.50;
+	salario = valor;
+
+	printf("Idade: %d\n", idade);
+	printf("salario: %f\n", salario);
+
+	return 0;
+
+}
+
+
